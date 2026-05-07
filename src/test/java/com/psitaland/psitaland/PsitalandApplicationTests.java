fbe19455a3dcf25b8444593669080dd0879fa9ef
Gustavo <gustavo.aranda01@fatec.sp.gov.br>
2026-05-07 12:41:38 -0300

    Deploy do projeto

---
+++ src/test/java/com/psitaland/psitaland/PsitalandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psitaland.psitaland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsitalandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
