2bf8715b48aab7bcac16b5dc17a09c3f401fee78
Gustavo <gustavo.aranda01@fatec.sp.gov.br>
2026-05-07 13:10:39 -0300

    feat: criação repository para gaiola e passaro

---
+++ src/main/java/com/psitaland/repositories/GaiolaRepository.java
@@ -0,0 +1,5 @@
+package com.psitaland.repositories;
+import com.psitaland.models.Gaiola;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GaiolaRepository extends JpaRepository<Gaiola, Integer> { }
---
+++ src/main/java/com/psitaland/repositories/PassaroRepository.java
@@ -0,0 +1,5 @@
+package com.psitaland.repositories;
+import com.psitaland.models.Passaro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassaroRepository extends JpaRepository<Passaro, Integer> { }
