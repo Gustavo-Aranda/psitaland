47ab7fd8779176ef957627cff1f1f0a380069961
Gustavo <gustavo.aranda01@fatec.sp.gov.br>
2026-05-21 11:29:55 -0300

    feat: criação repository de mutacao

---
+++ src/main/java/com/psitaland/repositories/MutacaoRepository.java
@@ -0,0 +1,5 @@
+package com.psitaland.repositories;
+import com.psitaland.models.Mutacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MutacaoRepository extends JpaRepository<Mutacao, Integer> { }
