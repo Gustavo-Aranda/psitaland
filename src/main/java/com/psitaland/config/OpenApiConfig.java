3f18b28f80bed0a3b74cf32d043f5ee2f61c1e1f
Gustavo <gustavo.aranda01@fatec.sp.gov.br>
2026-06-01 12:23:39 -0300

    feat: configuração do OpenApi

--- src/main/java/com/psitaland/config/OpenApiConfig.java
+++ src/main/java/com/psitaland/config/OpenApiConfig.java
@@ -1,4 +1,30 @@
 package com.psitaland.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuração do Swagger / SpringDoc OpenAPI.
+ *
+ * Acesse a documentação em: http://localhost:8080/swagger-ui.html
+ */
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Psitaland API")
+                        .description("API para gestão do plantel de aves do criadouro Psitaland. " +
+                                "Permite gerenciar pássaros, espécies, mutações, gaiolas e status.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Psitaland")
+                                .email("contato@psitaland.com")));
+    }
+
 }
